aa3c04f2f47e0124a4b5bac3fb42c104c8b5feb6
Ian Fletcher <IanF0347@gmail.com>
2022-01-11 09:52:19 -0700

    Added f1

--- Lucky7/Source.cpp
+++ Lucky7/Source.cpp
@@ -16,13 +16,31 @@ void f7();
 
 int main()
 {
-
+	f1();
 	return 0;
 }
-
+//Write program that reads 5 integers and prints the largest and smallest of the group
 void f1()
 {
-
+	int curr, small, large;
+	cout << "Enter 5 ints: ";
+	cin >> curr;
+	//first entry is small and large
+	small = large = curr;
+	for (int i = 0; i < 4; i++)
+	{
+		cin >> curr;
+		if (curr < small)
+		{
+			small = curr;
+		}
+		else if (curr > large)
+		{
+			large = curr;
+		}
+	}
+	cout << "Largest = " << large << " smallest = " << small << endl;
+	
 }
 
 void f2()
